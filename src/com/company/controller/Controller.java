655f73fa3c150b878bc0c35d3ec0836554668408
Earlied <31727544+Earlied@users.noreply.github.com>
2019-12-05 19:56:43 +0200

    Controller update

--- src/com/company/controller/Controller.java
+++ src/com/company/controller/Controller.java
@@ -1,4 +1,23 @@
 package com.company.controller;
 
+import com.company.model.Insurance;
+import com.company.model.Model;
+import com.company.view.View;
+
 public class Controller {
+    private Model model;
+    private View view;
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser(){
+        Insurance accidentInsurance = new Insurance("Accident insurance", "Josh Jackson");
+        view.print(accidentInsurance.toString());
+
+        accidentInsurance.sortByRisk();
+        view.print(accidentInsurance.toString());
+
+    }
 }
